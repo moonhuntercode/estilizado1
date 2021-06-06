08b6f06be0781e35aa101b243c6bcda19951f837
moonhunter <vdcloayza@gmail.com>
2021-06-06 16:34:43 -0400

    tratando de implementar tooltip

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,13 @@
-alert("works!");
+
+function greeting(name, role){
+    console.log(`Hello, my name is ${name}`);
+    console.log(`I'm a ${role}`);
+  }
+  
+  setTimeout(greeting, 2000, "Nathan", "Software developer");
+  const timeoutId = setTimeout(function(){
+    console.log("Hello World");
+}, 2000);
+
+clearTimeout(timeoutId);
+console.log(`Timeout ID ${timeoutId} has been cleared`);
